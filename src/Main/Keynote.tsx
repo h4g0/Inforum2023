cb256c124628499422e09797ecbad7f67071006a
Jácome Cunha <jacomecunha@mbp-de-jacome.lan>
2023-05-15 15:13:47 +0100

    adds Crista affiliation

--- src/Main/Keynote.tsx
+++ src/Main/Keynote.tsx
@@ -13,8 +13,7 @@ export function Keynote(props: any) {
 
                 
                 <h2>
-                <a href='https://www.ics.uci.edu/~lopes/'>Cristina Videira Lopes</a>
-                </h2>
+                <a href='https://www.ics.uci.edu/~lopes/'>Cristina Videira Lopes</a>, University of California, Irvine</h2>
 
                 <h2>O Fim da Programação (como a conhecemos)</h2>
                 <p>Mais detalhes em breve.</p>
