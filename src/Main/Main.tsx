78219a27919a76f42a2a611c63c0909922806a3b
h4g0 <afonsohugo24@gmail.com>
2023-07-23 17:06:09 +0100

    hidden navbar when scrolling

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -61,10 +61,37 @@ export function Login(this: any, props: any) {
                 </div>)
                 */
 
+            const [show, setShow] = useState(true);
+            const [lastScrollY, setLastScrollY] = useState(0);
+              
+            const controlNavbar = () => {
+                if (typeof window !== 'undefined') { 
+                    if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
+                    setShow(false); 
+                } else { // if scroll up show the navbar
+                      setShow(true);  
+                }
+              
+                // remember current page location to use in the next move
+                setLastScrollY(window.scrollY); 
+                }
+                };
+              
+                useEffect(() => {
+                  if (typeof window !== 'undefined') {
+                    window.addEventListener('scroll', controlNavbar);
+              
+                    // cleanup function
+                    return () => {
+                      window.removeEventListener('scroll', controlNavbar);
+                    };
+                  }
+                }, [lastScrollY]);
+    
 
     return (
         <Router>
-            <div className="Menu">
+            <div className={show ? "Menu" : "HiddenMenu"}>
 
             
 
